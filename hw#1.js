089422ba2479d63961ca1be1317f94a7df233e16
MarineIngenier <poeticcrane@gmail.com>
2024-01-10 11:28:06 +0300

    add counters

--- hw#1.js
+++ hw#1.js
@@ -3,13 +3,28 @@ const fs = require("fs");
 const HOSTNAME = "127.0.0.1";
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   if (req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     fs.createReadStream("./pages/index.html").pipe(res);
   } else if (req.url == "/about") {
-    console.log("about");
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     fs.createReadStream("./pages/about.html").pipe(res);
+  } else if (req.url == "/api/metrics") {
+    res.writeHead(200, { "Content-Type": "application/json;" });
+
+    fs.createReadStream("./metrics.json").pipe(res);
+  } else if (req.url == "/api/metrics-write") {
+    res.writeHead(200, { "Content-Type": "application/json;" });
+
+    req.on("data", function (data) {
+      const dataToWrite = JSON.stringify(JSON.parse(data));
+      fs.writeFileSync("metrics.json", dataToWrite);
+    });
+  } else if (req.url == "/api/jsrequest") {
+    res.writeHead(200, { "Content-Type": "text/javascript; charset=utf-8" });
+    fs.createReadStream("./metrics.js").pipe(res);
   } else {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     fs.createReadStream("./pages/error.html").pipe(res);
   }
 });
